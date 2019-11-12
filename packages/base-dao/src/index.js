e08d410983cb5fa98c31ccf531ab8a7c2074af63
Mark Polak <grishezz@gmail.com>
2019-11-13 00:10:51 +0200

    export runGenericDAOTests

--- packages/base-dao/src/index.js
+++ packages/base-dao/src/index.js
@@ -1,5 +1,7 @@
 const { createEntityDAO } = require('./baseDAO');
+const { runGenericDAOTests } = require('./../test/baseTestForDAOs');
 
 module.exports = {
-  createEntityDAO
+  createEntityDAO,
+  runGenericDAOTests
 };
