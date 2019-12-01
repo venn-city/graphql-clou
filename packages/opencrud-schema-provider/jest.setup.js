f15cebd047aa32aeab400a08c2ed82cff5b60b1c
Mark Polak <grishezz@gmail.com>
2019-12-01 11:12:31 +0200

    feat: allow configuring the db_port

--- packages/opencrud-schema-provider/jest.setup.js
+++ packages/opencrud-schema-provider/jest.setup.js
@@ -1,2 +1,5 @@
 process.env.IS_TEST = true;
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('dotenv').config({ path: `${process.cwd()}/../../.env` });
+
 jest.setTimeout(10000);
--- test/config.js
+++ test/config.js
@@ -1,10 +1,22 @@
+const dotenv = require("dotenv");
+initDotEnv(dotenv);
+
+function initDotEnv() {
+  const { error } = dotenv.config({ path: `${process.cwd()}/.env` });
+  if (error) {
+    console.warn("Did you create a '.env' file?");
+    throw new Error(error);
+  }
+}
+
 module.exports = {
   test: {
-    username: 'postgres',
-    password: 'postgres',
-    database: 'venn',
-    host: 'localhost',
-    dialect: 'postgres',
-    schema: 'venn'
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: "localhost",
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    schema: "venn"
   }
 };
