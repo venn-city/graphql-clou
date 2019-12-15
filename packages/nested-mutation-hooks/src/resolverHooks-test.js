ab5235d4d85d644d13007011e9ddaeb91d9e7d30
Segev Haviv <segevhaviv@gmail.com>
2019-12-15 21:26:11 +0200

    Fix lint

--- packages/nested-mutation-hooks/src/resolverHooks-test.js
+++ packages/nested-mutation-hooks/src/resolverHooks-test.js
@@ -2,9 +2,10 @@ const { sequelizeDataProvider: dataProvider } = require('@venncity/sequelize-dat
 const { createTestClient } = require('apollo-server-testing/dist/index');
 const { hacker } = require('faker');
 const sinon = require('sinon').createSandbox();
-const resolvers = require('./../test/graphqlTestServer/schema/resolvers');
 const { sq } = require('@venncity/sequelize-model');
+const resolvers = require('./../test/graphqlTestServer/schema/resolvers');
 const models = require('./../../../test/model');
+
 const mutationSpies = createMutationSpies();
 const { startApolloServer } = require('./../test/graphqlTestServer/');
 
