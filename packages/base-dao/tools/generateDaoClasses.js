c56e1b5f02acba9c12a4df84f46ffa970cb7ab91
shasaf1986 <asaf.shmueli@venn.city>
2020-01-01 15:13:59 +0200

    fix resolve file

--- packages/base-dao/tools/generateDaoClasses.js
+++ packages/base-dao/tools/generateDaoClasses.js
@@ -5,7 +5,7 @@ const ejs = require('ejs');
 const path = require('path');
 
 function generateDaoClass(entityName) {
-  return ejs.renderFile(path.resolve(__dirname, 'templates/DAOClass.ejs'), {
+  return ejs.renderFile(path.resolve(__dirname, 'templates/DaoClass.ejs'), {
     entityNameLowerFirstLetter: lowerFirst(entityName),
     entityNameUpperFirstLetter: upperFirst(entityName),
     entitiesNameLowerFirstLetter: lowerFirst(pluralize(entityName)),
