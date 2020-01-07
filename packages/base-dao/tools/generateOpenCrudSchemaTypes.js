69823cfd2e696cd9e8c06b1d5a4e237c42cb4021
shasaf1986 <asaf.shmueli@venn.city>
2020-01-07 17:29:36 +0200

    add missing id filed to entities

--- packages/base-dao/tools/generateOpenCrudSchemaTypes.js
+++ packages/base-dao/tools/generateOpenCrudSchemaTypes.js
@@ -1,18 +1,60 @@
-const prismaGenerateSchema = require('prisma-generate-schema');
+const { parseInternalTypes, generateCRUDSchemaFromInternalISDL } = require('prisma-generate-schema');
 const { plugin } = require('@graphql-codegen/typescript');
 const { codegen } = require('@graphql-codegen/core');
 const { printSchema, parse } = require('graphql');
+const { cloneField } = require('prisma-datamodel');
 
 function generateOpenCrudSchemaTypes(dataModel) {
-  const openCRUDSchema = prismaGenerateSchema.generateCRUDSchema(dataModel, 'postgres');
+  const schema = generateSchema(dataModel);
 
   return codegen({
-    schema: parse(printSchema(openCRUDSchema)),
+    schema,
     plugins: [{ typescript: {} }],
     documents: [],
     pluginMap: {
       typescript: { plugin }
     }
+  });
+}
+
+function generateSchema(dataModel) {
+  const sdl = parseInternalTypes(dataModel, 'postgres');
+  sdl.types.forEach(({ isEnum, fields }) => {
+    if (isEnum) {
+      return;
+    }
+    fields.forEach(({ relatedField, isRequired, isList, name }) => {
+      if (relatedField === null || isList) {
+        return;
+      }
+      const additionalIdField = generateAdditionalIdField(name, isRequired);
+      fields.push(additionalIdField);
+    });
+  });
+  const schema = generateCRUDSchemaFromInternalISDL(sdl, 'postgres');
+
+  return parse(printSchema(schema));
+}
+
+function generateAdditionalIdField(originalFieldName, isRequired) {
+  return cloneField({
+    name: `${originalFieldName}Id`,
+    relatedField: null,
+    isList: false,
+    type: 'ID',
+    relationName: null,
+    defaultValue: null,
+    isUnique: false,
+    isRequired,
+    isId: true,
+    idStrategy: null,
+    associatedSequence: null,
+    isUpdatedAt: false,
+    isCreatedAt: false,
+    isReadOnly: true,
+    databaseName: null,
+    directives: [],
+    comments: []
   });
 }
 
