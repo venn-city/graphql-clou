a094632a57aa873c9476a658204f98a1abaccee2
Mark Polak <grishezz@gmail.com>
2020-01-09 16:55:07 +0200

    build: update to sequelize venncity fork as npm package instead of git dependency
    also upgrade to node12

--- packages/graphql-transformers/src/openCrudWhereTransform/manyRelationConditionSubquery.js
+++ packages/graphql-transformers/src/openCrudWhereTransform/manyRelationConditionSubquery.js
@@ -1,4 +1,4 @@
-const Sequelize = require('sequelize');
+const Sequelize = require('@venncity/sequelize');
 const { trimEnd, lowerFirst } = require('lodash');
 const sequelizeConsts = require('./sequelizeConsts');
 
--- packages/sequelize-model/src/base/baseModel.js
+++ packages/sequelize-model/src/base/baseModel.js
@@ -1,4 +1,4 @@
-const Sequelize = require('sequelize');
+const Sequelize = require('@venncity/sequelize');
 
 const baseModel = {
   id: {
