3a1a6e3559e4fb1a0f7416203624c2acd2f11137
omermyaari <omermyaari@gmail.com>
2020-03-23 13:15:18 +0200

    fix(base-dao): export basedao functions that werent exported to index

--- packages/base-dao/src/index.js
+++ packages/base-dao/src/index.js
@@ -1,8 +1,10 @@
-const { createEntityDAO } = require('./baseDAO');
+const { createEntityDAO, getFunctionNamesForEntity, transformJoinedEntityWhere } = require('./baseDAO');
 const { runGenericDAOTests, BASE_DAO_TEST_TYPES } = require('./../test/baseTestForDAOs');
 
 module.exports = {
   createEntityDAO,
   runGenericDAOTests,
+  getFunctionNamesForEntity,
+  transformJoinedEntityWhere,
   BASE_DAO_TEST_TYPES
 };
