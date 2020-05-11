72b6c4e81cf2746cb4ee54ff0ee3da9ff4ef5818
Mark Polak <grishezz@gmail.com>
2020-05-11 11:25:05 +0300

    close sequelize after all tests

--- packages/graphql-transformers/jest.setup.js
+++ packages/graphql-transformers/jest.setup.js
@@ -1,2 +1,9 @@
+process.env.CLS_ENABLED = true;
 process.env.IS_TEST = true;
 jest.setTimeout(10000);
+
+const sequelizeModel = require('@venncity/sequelize-model');
+
+afterAll(async () => {
+  await sequelizeModel.sq.sequelize.close();
+});
