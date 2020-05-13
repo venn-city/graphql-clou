02a53ded768844a8c32c35f96784bb52c0e926a9
Mark Polak <grishezz@gmail.com>
2020-05-13 11:16:54 +0300

    try to disable in-memory pagination - a test is failing

--- packages/graphql-transformers/src/resultLimiter/resultLimiter.js
+++ packages/graphql-transformers/src/resultLimiter/resultLimiter.js
@@ -4,7 +4,8 @@ const { sq } = require('@venncity/sequelize-model');
 const { BELONGS_TO_MANY } = require('./../openCrudWhereTransform/sequelizeConsts').RELATION_TYPES;
 
 function shouldLimitInFetch(args) {
-  return args && !containsEveryOrNone(args.where);
+  return true;
+  // return args && !containsEveryOrNone(args.where);
 }
 
 function containsEveryOrNone(where) {
@@ -12,27 +13,26 @@ function containsEveryOrNone(where) {
 }
 
 function limitAfterFetch(args, fetchedEntities) {
-  if (args && (args.first || args.skip)) {
-    const start = args.skip || 0;
-    const end = (args.first || 0) + start;
-    fetchedEntities = fetchedEntities.slice(start, end);
-  }
+  // if (args && (args.first || args.skip)) {
+  //   const start = args.skip || 0;
+  //   const end = (args.first || 0) + start;
+  //   fetchedEntities = fetchedEntities.slice(start, end);
+  // }
   return fetchedEntities;
 }
 
 // TODO: for now omit these args as sequelize does not support them in nested nXm relations.
 // https://github.com/sequelize/sequelize/issues/4376
 function omitLimitArgsIfRequired(entityName, relationFieldName, args) {
-  const associationInfo = Object.values(sq[upperFirst(entityName)].associations).find(association => association.as === relationFieldName);
-  if (args && associationInfo.associationType === BELONGS_TO_MANY) {
-    return omit(args, ['first', 'skip']);
-  }
+  // const associationInfo = Object.values(sq[upperFirst(entityName)].associations).find(association => association.as === relationFieldName);
+  // if (args && associationInfo.associationType === BELONGS_TO_MANY) {
+  //   return omit(args, ['first', 'skip']);
+  // }
   return args;
 }
 
 module.exports = {
   shouldLimitInFetch,
-  containsEveryOrNone,
   limitAfterFetch,
   omitLimitArgsIfRequired
 };
