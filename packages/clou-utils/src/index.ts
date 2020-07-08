ee65797b8b76ffec7b2a96d3990e34ca44279e6c
Segev Haviv <segevhaviv@gmail.com>
2020-07-08 12:43:21 +0300

    Types fix

--- packages/clou-utils/src/index.ts
+++ packages/clou-utils/src/index.ts
@@ -1,7 +1,30 @@
-import transformWithSymbols from './transformWithSymbols';
+import transformWithSymbolsOriginal from './transformWithSymbols';
 import generalUtils from './generalUtils';
+
+const {
+  generateRandomString: generateRandomStringOriginal,
+  generateRandomInteger: generateRandomIntegerOriginal,
+  isTrue: isTrueOriginal,
+  isFalse: isFalseOriginal,
+  encrypt: encryptOriginal,
+  decrypt: decryptOriginal,
+  getAdditionalInfoFromRequest: getAdditionalInfoFromRequestOriginal,
+  extractWhereFromFederationReference: extractWhereFromFederationReferenceOriginal,
+  isEmptyArray: isEmptyArrayOriginal
+} = generalUtils;
+
+export const generateRandomString = generateRandomStringOriginal;
+export const generateRandomInteger = generateRandomIntegerOriginal;
+export const isTrue = isTrueOriginal;
+export const isFalse = isFalseOriginal;
+export const encrypt = encryptOriginal;
+export const decrypt = decryptOriginal;
+export const getAdditionalInfoFromRequest = getAdditionalInfoFromRequestOriginal;
+export const extractWhereFromFederationReference = extractWhereFromFederationReferenceOriginal;
+export const isEmptyArray = isEmptyArrayOriginal;
+export const transformWithSymbols = transformWithSymbolsOriginal;
 
 export default {
   ...generalUtils,
-  transformWithSymbols
+  transformWithSymbols: transformWithSymbolsOriginal
 };
